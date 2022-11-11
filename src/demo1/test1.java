6f00cbf42b7b24d36e38b41fbfddef1db781111c
Khanorkar <Khanorkar@DESKTOP-JM76D50>
2022-11-11 17:32:47 +0530

    commit 1

---
+++ src/demo1/test1.java
@@ -0,0 +1,16 @@
+package demo1;
+
+public class test1 
+{
+public static void main(String[] args)
+{
+	System.out.println("akshay khanorkar");
+	
+	M1();
+}
+
+public static void M1()
+{
+	System.out.println("hello");
+}
+}
